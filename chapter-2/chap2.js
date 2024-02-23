54cacbee0c6d116d09801e73778f7b3d43a58156
RUMI <rizwanfahad812@gmail.com>
2024-02-23 11:46:03 +0500

    chapter two done

---
+++ chapter-2/chap2.js
@@ -0,0 +1,23 @@
+// Q1
+var username = ""
+alert (username)
+// Q2
+var myname = "RUMI-BIN-FAHAD"
+alert (myname)
+// Q3
+var titleMessage = "Salam un Alikum World"
+alert (titleMessage)
+// Q4
+var studentData = "Certified Mobile Application Developer"
+alert (studentData)
+// Q5
+var breakline = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert (breakline)
+// Q6
+var email = "My Email Address\nrizwanfahad812@gmail.com"
+alert (email)
+// Q7
+// document.getElementById ("#demo")
+// Q8
+var emoji = "❤-----------👀------------❤"
+alert (emoji)
