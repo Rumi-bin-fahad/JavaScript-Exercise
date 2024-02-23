e8390d9f4ad754d5032f390b6283c8445a0d1766
RUMI <rizwanfahad812@gmail.com>
2024-02-23 11:26:32 +0500

    chapter o1e done

---
+++ chapter-1/chap1.js
@@ -0,0 +1,10 @@
+
+// Q1
+alert("SALAM-UN-ALIKUM")
+// Q2
+alert ("JAVASCRIPT ALERT\nError! Please Enter a Valid Password")
+// Q3
+alert ("Welcome to JS Land\nHappy Coding!")
+alert ("Welcome to JS Land ! Happy Codding!")
+// Q4
+alert ("Happy Coding!\nPrevent this page from creating additional dialogs")
