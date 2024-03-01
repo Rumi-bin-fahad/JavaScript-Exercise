40602508c0ced6889c83c54872cf2a96e46ab3be
RUMI <rizwanfahad812@gmail.com>
2024-03-01 15:57:27 +0500

    chapter five done

---
+++ chapter-3/chap3.js
@@ -0,0 +1,12 @@
+// Q1
+var age = "I am 15 Years Old"
+alert (age)
+// Q2
+var vistingTimes = "You Have Visited this site 14 times"
+alert (vistingTimes)
+// Q3
+document.write ("<h2> My Birth Year is 2006</h2><p> Data Type of Declare Variable is number </p>")
+// Q4
+var visitorName = "RUMI"
+var ProductTitle = "Eye Wear"
+var Quantity = "2 Blue Cut"
---
+++ chapter-5/chap5.js
@@ -0,0 +1,88 @@
+
+
+// Q1
+let value1 = 10;
+let value2 = 15;
+// addition
+let addition = value1 + value2;
+console.log("Sum of Value1 and Value2 is", addition);
+// subctraction
+let sub = value1 - value2;
+console.log("Subtraction of Value1 and Value2 is", sub);
+// multiplication
+let mul = value1 * value2;
+console.log("multiplication of Value1 and Value2 is", mul);
+// division
+let div = value1 / value2;
+console.log("division of Value1 and Value2 is", div);
+// modulus
+let modulus = value1 % value2;
+console.log("Reminder..Modulus of Value1 and Value2 is", modulus);
+
+// Q2
+let known = "";
+console.log("Known Variable value is undefined");
+let initial = 5;
+let final = 6;
+addition = initial + final;
+console.log("Initial Value of initial is", initial);
+initial++;
+console.log("Value After Increment is", initial);
+console.log("Addition Value of Final and Initial variable is",addition);
+addition--;
+console.log("The value of addition decrement is", addition)
+console.log("The reminder of initial and final is",initial % final)
+
+// Q3
+let oneTicket = 600;
+let fiveTicket = 5;
+ticketCost = (oneTicket * fiveTicket);
+console.log("The total sum of 5 Movies ticket is", ticketCost,"PKR");
+
+// Q4
+let one = 1;
+two = 2;
+three = 3;
+four = 4;
+five = 5;
+six = 6;
+seven = 7;
+eight = 8;
+nine = 9;
+ten = 10;
+let table = 13;
+console.log("13 X 1 =",one*table);
+console.log("13 X 2 =",two*table);
+console.log("13 X 3 =",three*table);
+console.log("13 X 4 =",four*table);
+console.log("13 X 5 =",five*table);
+console.log("13 X 6 =",six*table);
+console.log("13 X 7 =",seven*table);
+console.log("13 X 8 =",eight*table);
+console.log("13 X 9 =",nine*table);
+console.log("13 X 10 =",ten*table);
+
+// Q5
+let item1 = 564;
+let item2 = 200;
+let OrderQuantity = 4;
+let OrderQuantity2 = 9;
+totalQuantity = OrderQuantity + OrderQuantity2;
+let ShippingCharges = 100.324;
+totalsum = item1 + item2 + ShippingCharges;
+console.log("Price of Item 1 is",item1);
+console.log("Price of Item 2 is", item2);
+console.log("Order Quantity of Item 1 is ", OrderQuantity);
+console.log("Order Quantity of Item 2 is", OrderQuantity2);
+console.log("Shipping Charges Including Taxes",ShippingCharges);
+console.log("Total Order Quantity of Item 1 and Item 2 is",totalQuantity);
+console.log("Total Amount of Item 1 and Item 2 including taxes is",totalsum);
+
+
+// Q6
+let TM = 1050;
+let OM = 1100;
+let Percentage = (OM - TM)/(TM)*100;
+console.log("total marks is", TM);
+console.log("Obtained Makrs is",OM);
+console.log("Percentage of the Student is",Percentage);
