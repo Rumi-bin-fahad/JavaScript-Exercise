bbf2b3984f571be4f89b38efa452826758a213cc
RUMI <rizwanfahad812@gmail.com>
2024-05-15 10:49:10 +0500

    event listener added

---
+++ EventListner/app.js
@@ -0,0 +1,43 @@
+let MainImg = document.getElementById('main-Img');
+//we call main image element in a variable
+
+let img1 = document.getElementById('img1');
+img1.addEventListener('click', function () {
+    MainImg.src = 'https://plus.unsplash.com/premium_photo-1661835557655-99a912d26132?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGNvd3N8ZW58MHx8MHx8fDA%3D', MainImg.style.border = "2px solid black", img1.style.border = "3px solid black"
+});
+img1.addEventListener('mouseleave', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img1.style.border = "none",alert("Bhai Australian Hoooo Tery Budget my Nh aounga\nKoi or Dekh ly")
+})
+let img2 = document.getElementById('img2');
+img2.addEventListener('click', function () {
+    MainImg.src = 'https://plus.unsplash.com/premium_photo-1661895100691-06cf2364d0b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGNvd3N8ZW58MHx8MHx8fDA%3D', img2.style.border = "3px solid black"
+});
+img2.addEventListener('mouseleave', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img2.style.border = "none"
+    ,alert("Bhai Ma Shal Allah Bol dy \nNazar Lagaye ga Kia 👀")
+})
+let img3 = document.getElementById('img3');
+img3.addEventListener('click', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1546445317-29f4545e9d53?q=80&w=2004&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img3.style.border = "3px solid black"
+});
+img3.addEventListener('mouseleave', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img3.style.border = "none"
+    ,alert("Bhai Mujhy Kharid Loo Mein Razi Hoo \nBaat Kro kia Malik sy 😁")
+    
+})
+let img4 = document.getElementById('img4');
+img4.addEventListener('click', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1550348579-959785e820f7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29hdHxlbnwwfHwwfHx8MA%3D%3D', img4.style.border = "3px solid black"
+});
+img4.addEventListener('mouseleave', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img4.style.border = "none"
+    ,alert("Bhai Jori Hye Hamari Baraber Wali Ko Lazmi Lena parhy ga \nWrna Soda Cancel 😎")
+})
+let img5 = document.getElementById('img5');
+img5.addEventListener('click', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1560888126-5c13ad3f9345?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGdvYXR8ZW58MHx8MHx8fDA%3D', img5.style.border = "3px solid black"
+});
+img5.addEventListener('mouseleave', function () {
+    MainImg.src = 'https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?q=80&w=1988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', img5.style.border = "none"
+    ,alert("Bhai Mera Gosht Saylani My dy dena❤")
+})
