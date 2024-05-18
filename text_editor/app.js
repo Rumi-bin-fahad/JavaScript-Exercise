f44d91626c72809d14f69bfef436c72bce274b09
RUMI <rizwanfahad812@gmail.com>
2024-05-18 12:15:28 +0500

    files added and text editor

---
+++ text_editor/app.js
@@ -0,0 +1,44 @@
+// let heading = document.getElementById('heading');
+// let text = document.getElementById('text');
+
+let para = document.getElementById('para')
+function bold(){
+    // text.style.fontWeight='800'
+    if(para.style.fontWeight !== '800')
+    para.style.fontWeight='800'
+    else{
+        para.style.fontWeight='400' 
+    }
+}
+function Italic(){
+    if(para.style.fontStyle !== 'Italic')
+    para.style.fontStyle='Italic'
+    else{
+        para.style.fontStyle='normal' 
+    }
+}
+function Underline(){
+    if(para.style.textDecoration !== 'underline')
+    para.style.textDecoration='underline'
+    else{
+        para.style.textDecoration='none' 
+    }
+}
+function align_left(){
+    if(para.style.textAlign !== 'left')
+    para.style.textAlign='left'
+    else{
+        para.style.textAlign='center' 
+    }
+}
+function align_Right(){
+    if(para.style.textAlign !== 'right')
+    para.style.textAlign='right'
+    else{
+        para.style.textAlign='center' 
+    }
+}
+let color_change = document.getElementById('color1');
+function color_change1(){
+    para.style.color = color_change.value
+}
